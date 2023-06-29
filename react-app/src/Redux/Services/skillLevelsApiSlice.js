ccbe8a2d0dc7f1c4d6a1f33125b0e07dce5505cc
brian.mckenna <brian.mckenna@datto.com>
2023-06-29 11:54:49 +0100

    add tag types for skill levels

--- react-app/src/Redux/Services/skillLevelsApiSlice.js
+++ react-app/src/Redux/Services/skillLevelsApiSlice.js
@@ -1,11 +1,14 @@
 import { apiSlice } from "./apiSlice";
 
-export const skillLevelsApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    getAllSkillLevels: builder.query({
-      query: () => "/skilllevels",
+export const skillLevelsApiSlice = apiSlice
+  .enhanceEndpoints({ addTagTypes: ["SkillLevels"] })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      getAllSkillLevels: builder.query({
+        query: () => "/skilllevels",
+        providesTags: ["SkillLevels"],
+      }),
     }),
-  }),
-});
+  });
 
 export const { useGetAllSkillLevelsQuery } = skillLevelsApiSlice;
